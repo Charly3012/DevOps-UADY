95744c26058e6235540b88b429e547d3b1a44e1a
Carlos Coronado <ccoronado@nasa.com.mx>
2025-02-26 18:51:52 -0600

    Feat: Add automatization to PaginaWeb

---
+++ Script Automatización de pagina web/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo systemctl stop nginx
+pkill ngrok
+
+
+cd /var/www || exit
+
+if [ ! -d "DevOps-UADY" ]; then
+    sudo git clone https://github.com/Charly3012/DevOps-UADY
+    echo "Repositorio clonado."
+else
+    cd DevOps-UADY || exit
+    sudo git pull origin main || { echo "Error al actualizar el repositorio."; exit 1; }
+    echo "Repositorio actualizado."
+fi
+
+
+sudo rm -rf /var/www/html/*
+cd DevOps-UADY/PaginaWeb || exit
+sudo cp -r . /var/www/html/
+
+
+sudo systemctl start nginx || { echo "Error iniciando el servidor web"; exit 1; }
+
+ngrok http 80 --log=stdout &> /tmp/ngrok.log &
+
+sleep 5
+
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
+
+echo "URL de la pagina desplegada: $NGROK_URL"
